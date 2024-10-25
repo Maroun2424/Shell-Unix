96324f1817b66d7f18252be9a992104c39e0116a
Arkam <rayane.arkam@etu.u-paris.fr>
2024-10-25 15:24:12 +0200

    Ajout de exit

---
+++ test/exit_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../include/commands.h"
+
+int main() {
+    printf("Test de la fonction exit:\n");
+    
+    printf("Appel de  cmd_exit(0)...\n");
+    cmd_exit(0);  // Le programme doit se terminer ici avec le code 0
+    
+    // Si le programme continue après l'appel à exit, c'est qu'il y a un problème
+    printf("Erreur: cmd_exit() n'a pas arrêté le programme!\n");
+    return 1;
+}
