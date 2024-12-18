e184a0aed4db6d9465776828f8e97bedaf302d57
nabti <paul.nabti@etu.u-paris.fr>
2024-12-18 18:18:01 +0100

    For valeur de retour

--- include/for.h
+++ include/for.h
@@ -19,7 +19,7 @@ int initialize_loop(char *args[], for_loop_t *loop);
 int parse_command_block(char *args[], int start_index, for_loop_t *loop);
 int should_include_file(const struct dirent *entry, const for_loop_t *loop);
 void generate_command_with_substitution(const char *template, const char *file_path, char *cmd_buffer, size_t buffer_size);
-void process_directory(const for_loop_t *loop);
+int process_directory(const for_loop_t *loop);
 int simple_for_loop(char *args[]);
 
 #endif // FOR_H
