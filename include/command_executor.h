5ff4f8e7e4e29cd83d4830517d95bc8f3f409b4d
nabti <paul.nabti@etu.u-paris.fr>
2024-12-19 21:14:01 +0100

    Fichier d'en tête, comparaison int < unsigned int

--- include/command_executor.h
+++ include/command_executor.h
@@ -2,7 +2,7 @@
 #define COMMAND_EXECUTOR_H
 
 extern int last_exit_status;
+extern int if_test_mode;
 void process_command(const char *input);
-void process_sequential_commands(const char *input);
 
 #endif // COMMAND_EXECUTOR_H
